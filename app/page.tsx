52b7a749da7b5003efb96a65697645b4fa3d6722
Carlos Esposito <elcharlybeto@hotmail.com>
2024-11-20 13:23:55 -0300

    minor fonts changes

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="min-h-screen flex items-center justify-center ">
       <main className="flex flex-col gap-8 items-center">
         <Image src='/img/shopping-cart.svg' width={150} height={150} alt="carrito"/>
-        <h1 className="font-bold text-3xl">Don´t U forget anything!</h1>
+        <h1 className="font-bold text-center style={{ fontFamily: 'var(--font-sour-gummy)' }}text-3xl">Don´t U forget anything!</h1>
       </main>
     </div>
   );
