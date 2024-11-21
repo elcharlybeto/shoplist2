1a7bfa590cb0df9e04224c02a2db82636024f07f
Carlos Esposito <elcharlybeto@hotmail.com>
2024-11-21 16:49:37 -0300

    creating pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "@/app/ui/global.css";
 import type { Metadata } from "next";
 import { MyContextProvider } from "./lib/myContext";
 import NavBar from "./ui/navbar";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "ShopList",
@@ -16,6 +17,11 @@ export default function RootLayout({
 
   return (
     <html lang="es">
+       <Head>
+        <meta name="theme-color" content="#4a90e2" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/icons/icon-128x128.png" />
+      </Head>
       <body>
         <MyContextProvider>
         <div className="fixed top-0 left-0 w-full z-10"><NavBar /></div>
--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,15 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+import nextPWA from 'next-pwa';
+
+export const withPWA = nextPWA({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  });
+  
+//   module.exports = withPWA({
+//     reactStrictMode: true,
+//   });
